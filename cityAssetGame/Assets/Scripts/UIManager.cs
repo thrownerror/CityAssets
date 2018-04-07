585a69c3475cfee70f4973b76164b8e7bad57d1b
nitvic793 <nithishvictor@gmail.com>
2018-04-07 18:51:29 -0400

    UI [In Progress]

---
+++ cityAssetGame/Assets/Scripts/UIManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIManager : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public GameObject storeImage;
+    public GameObject houseImage;
+    public GameObject domeImage;
+
+    public List<GameObject> imageInstances;
+    public bool isShowingMenu = false;
+
+    bool isOver;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ShowMenu()
+    {
+        var instance = Instantiate(storeImage, transform.parent);
+        imageInstances.Add(instance);
+        var x = instance.transform.position.x;
+        x += 40;
+        var pos = instance.transform.position;
+        instance.transform.position = new Vector3(x, pos.y, pos.z);
+        instance = Instantiate(domeImage, transform.parent);
+        imageInstances.Add(instance);
+        pos = instance.transform.position;
+        x += 50;
+        instance.transform.position = new Vector3(x, pos.y, pos.z);
+        instance = Instantiate(houseImage, transform.parent);
+        imageInstances.Add(instance);
+        pos = instance.transform.position;
+        x += 50;
+        instance.transform.position = new Vector3(x, pos.y, pos.z);
+    }
+
+    void CollapseMenu()
+    {
+        foreach(var item in imageInstances)
+        {
+            DestroyImmediate(item);
+        }
+        imageInstances.Clear();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click");
+        isShowingMenu = !isShowingMenu;
+        if(isShowingMenu)
+        {
+            ShowMenu();
+        }
+        else
+        {
+            CollapseMenu();
+        }     
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Mouse enter");
+        isOver = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Mouse exit");
+        isOver = false;
+    }
+}
