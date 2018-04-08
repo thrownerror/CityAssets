032cba2b7adde5a7a1448d058da3a259f449d371
rahul <rsv1804@g.rit.edu>
2018-04-08 16:19:44 -0400

    Menu Scene

---
+++ cityAssetGame/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+    public Button creditsButton;
+    public GameObject screen;
+
+    // Use this for initialization
+    void Start () {
+
+
+        Button btn = creditsButton.GetComponent<Button>();
+        screen.SetActive(false);
+        btn.onClick.AddListener(OpenCredits);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        if (Input.GetKeyDown("escape"))
+        {
+            screen.SetActive(false);
+        }
+
+	}
+
+    void OpenCredits()
+    {
+        screen.SetActive(true);
+    }
+}
